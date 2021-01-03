d9d83f55ad4868c7809527010f094c7ef63b53f4
Veysel Usta <veyselusta7@gmail.com>
2021-01-03 18:52:33 +0300

    Set hours

--- 02Clock/index.js
+++ 02Clock/index.js
@@ -1,8 +1,9 @@
 const secondHand = document.querySelector('.second-hand')
 const minHand = document.querySelector('.min-hand')
-
+const hourHand = document.querySelector('.hour-hand')
 
 function setDate(){
+
   const now = new Date()
 
   const seconds = ((now.getSeconds()/60)*360)
@@ -11,6 +12,10 @@ function setDate(){
   const minute = ((now.getUTCMinutes()/60)*360)
   minHand.style.transform = `rotate(${minute}deg)`
   
+  const hour = ((now.getHours()/12)*360)
+  hourHand.style.transform = `rotate(${hour}deg)`
+  
+  console.log(now.getUTCHours())
 
 }
 
