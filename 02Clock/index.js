dd00e83c5eef1258957455f6367e8346797f76d0
Veysel Usta <veyselusta7@gmail.com>
2021-01-03 18:46:34 +0300

    Set the second of the clock and apply it to the animation

--- 02Clock/index.js
+++ 02Clock/index.js
@@ -1,5 +1,16 @@
+const secondHand = document.querySelector('.second-hand')
+const minHand = document.querySelector('.min-hand')
+
+
 function setDate(){
-  console.log('hi')
+  const now = new Date()
+
+  const seconds = ((now.getSeconds()/60)*360)
+  secondHand.style.transform = `rotate(${seconds}deg)`
+
+ 
+  
+
 }
 
-//setInterval(setDate, 1000);
+setInterval(setDate, 1000);
