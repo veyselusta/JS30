3efdf357c0a0066723d61f691902932502f9c6c1
Veysel Usta <veyselusta7@gmail.com>
2021-01-05 16:14:05 +0300

    Change css variables

--- 03CSS/index.js
+++ 03CSS/index.js
@@ -1,7 +1,9 @@
 const input = document.querySelectorAll('.controls input')
 
 function handle(){
-  console.log(this.value)
+  const suffix = this.dataset.sizing || ''
+  document.documentElement.style.setProperty(`--${this.name}`,this.value + suffix)
 }
 
 input.forEach(input=>input.addEventListener('change',handle))
+input.forEach(input=>input.addEventListener('mousemove',handle))
